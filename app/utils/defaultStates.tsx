9ee6fe01bbaa84653f7ed39f73c2a540edcfd94a
sridhar <sridzar1234@gmail.com>
2024-05-28 12:47:42 +0530

    first commit

---
+++ app/utils/defaultStates.tsx
@@ -0,0 +1,39 @@
+const defaultStates = [
+  {
+    name: "Coimbatore",
+    country: "IND",
+    state: "Tamilnadu",
+    lat: 11.0168,
+    lon: 76.9558,
+  },
+  {
+    name: "Chennai",
+    country: "IND",
+    state: "Tamilnadu",
+    lat: 13.0843,
+    lon: 80.2705,
+  },
+  {
+    name: "Bangalore",
+    country: "IND",
+    state: "Karnataka",
+    lat: 12.9716,
+    lon: 77.5946,
+  },
+  {
+    name: "varkala",
+    country: "IND",
+    state: "Kerala",
+    lat: 8.737868,
+    lon: 76.716339,
+  },
+  {
+    name: "Poombarai",
+    country: "IND",
+    state: "Kodaikanal",
+    lat: 10.2564,
+    lon: 77.4078,
+  },
+];
+
+export default defaultStates;
